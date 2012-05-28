d3479a04676162d1ff4dde9257e2446466dccd9e
Denis I. Lukyanov <d.lukyanov@secoint.ru>
2012-05-29 01:49:39 +0400

    Translations moved to ru.yml + some visual refactoring.

--- app/overrides/downloadable_links.rb
+++ app/overrides/downloadable_links.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path  => "products/show",
-            :insert_after => "div#product-description",
+            :insert_after => "[data-hook='product_description']",
             :partial      => "products/downloadable_links",
             :name         => "downloadable_links")
 
