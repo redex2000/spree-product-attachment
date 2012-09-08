d040f65ccc437dc4f009b0b9609defdfea7282c7
Service & Consulting <service@secoint.ru>
2012-09-09 01:14:50 +0400

    Removed deprecation warning.
    Removed deprecation warning "don't put space before argument parentheses".

--- app/helpers/product_attachments_helper.rb
+++ app/helpers/product_attachments_helper.rb
@@ -4,9 +4,9 @@ module ProductAttachmentsHelper
     extension_name = File.extname(downloadable.attachment_file_name)
     extension_name.slice!(0)
     extension_name.downcase!
-    link = link_to (image_tag("attachment_extension/icons/#{extension_name}.gif"), downloadable.attachment.url(:product), :title => downloadable.description)
+    link = link_to(image_tag("attachment_extension/icons/#{extension_name}.gif"), downloadable.attachment.url(:product), :title => downloadable.description)
     link << " "
-    link << link_to (downloadable.title, downloadable.attachment.url(:product), :title => downloadable.description)
+    link << link_to(downloadable.title, downloadable.attachment.url(:product), :title => downloadable.description)
     link << " (#{number_to_human_size(downloadable.attachment_file_size)})"
 
     link
